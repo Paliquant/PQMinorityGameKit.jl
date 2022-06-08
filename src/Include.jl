48861e81e10143a8583205c9b7a69ee9fc8dc1a9
Jeffrey Varner <jvarner@paliquant.com>
2022-06-08 14:13:08 -0400

    impl
    impl

--- src/Include.jl
+++ src/Include.jl
@@ -4,6 +4,7 @@ const _PATH_TO_BASE = joinpath(_PATH_TO_SRC, "base")
 
 # load external packages that are required for the simulation
 using Random
+using DataFrames
 
 # set the seed -
 Random.seed!(Random.make_seed())
--- src/base/Engine.jl
+++ src/base/Engine.jl
@@ -1,19 +1,24 @@
 
 # Basic simulation -
-function _simulation(worldModel::PQBasicMinorityGameKitWorld, context::PQBasicMinorityGameKitSimulationContext)
+function _simulation(world::PQBasicMinorityGameKitWorld, context::PQBasicMinorityGameKitSimulationContext)::Dict{Int64,DataFrame}
 
     # initialize -
     numberOfSimulationSteps = context.numberOfSimulationSteps
+    agentArray = world.agentArray
+    simulationStateArchive = Dict{Int64,DataFrame}()
 
     # main loop -
     for s ∈ 1:numberOfSimulationSteps
     end
 
+
+    # return -
+    return simulationStateArchive
 end
 
 
 # this method dispatches to context specific simulation impls -
-function run(world::T) where T <: PQAbstractGameWorld
+function run(world::T)::Dict{Int64,DataFrame} where T <: PQAbstractGameWorld
 
     # get context -
     context = world.context
